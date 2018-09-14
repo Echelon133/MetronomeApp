26b7cb88fafc52258f9127084ddd36a3596447ab
Mateusz Bednarski <echelon133@gmail.com>
2018-09-14 17:26:48 +0200

    Added TimeSignatureHolder

---
+++ src/main/java/ml/echelon133/metronome/TimeSignatureHolder.java
@@ -0,0 +1,32 @@
+package ml.echelon133.metronome;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TimeSignatureHolder {
+    private static Map<String, Integer> timeSignatures;
+
+    static {
+        timeSignatures.put("2/4", 2);
+        timeSignatures.put("3/4", 3);
+        timeSignatures.put("4/4", 4);
+        timeSignatures.put("5/4", 5);
+        timeSignatures.put("6/4", 6);
+        timeSignatures.put("7/4", 7);
+    }
+
+    public static List<String> getAllSignatureNames() {
+        List<String> names = timeSignatures
+                .entrySet()
+                .stream()
+                .sorted()
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        return names;
+    }
+
+    public static Integer getNumberOfBeatsInSignature(String name) {
+        return timeSignatures.get(name);
+    }
+}

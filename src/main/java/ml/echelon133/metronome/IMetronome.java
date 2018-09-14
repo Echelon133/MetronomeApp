badfa6d33ea5927f7adaffc96e066f612e04a6b9
Mateusz Bednarski <echelon133@gmail.com>
2018-09-14 16:56:05 +0200

    Added IMetronome interface

---
+++ src/main/java/ml/echelon133/metronome/IMetronome.java
@@ -0,0 +1,11 @@
+package ml.echelon133.metronome;
+
+public interface IMetronome {
+    void incrementBPMByOne();
+    void decrementBPMByOne();
+    void incrementBPMByFive();
+    void decrementBPMByFive();
+    long calculatePause();
+    void setAccentInterval(Integer accentInterval);
+    Integer getBPM();
+}

269173ca17ad764357105a86a1e0a21b586e35fa
Mateusz Bednarski <echelon133@gmail.com>
2018-09-22 22:13:07 +0200

    Now Main class loads sound files from resources

--- src/main/java/ml/echelon133/Main.java
+++ src/main/java/ml/echelon133/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,6 +12,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/ml/echelon133/fxml/metronome.fxml"));
+        Media strongBeat = new Media(getClass().getResource("/ml/echelon133/sounds/strong_beat.mp3").toExternalForm());
+        Media weakBeat = new Media(getClass().getResource("/ml/echelon133/sounds/weak_beat.mp3").toExternalForm());
+
         primaryStage.setTitle("MetronomeApp");
         primaryStage.setScene(new Scene(root, 360, 225));
         primaryStage.show();

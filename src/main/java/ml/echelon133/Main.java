faf7d54088b5822072a8ba91e89d25fe99284b7a
Mateusz Bednarski <echelon133@gmail.com>
2018-09-25 19:48:54 +0200

    Main class update that makes freshly added progressbar functionality work

--- src/main/java/ml/echelon133/Main.java
+++ src/main/java/ml/echelon133/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.media.Media;
 import javafx.stage.Stage;
 import ml.echelon133.metronome.ClickPlayer;
+import ml.echelon133.metronome.MeasureProgress;
 import ml.echelon133.metronome.Metronome;
 import ml.echelon133.metronome.listener.IClickListener;
 
@@ -20,10 +21,19 @@ public class Main extends Application {
         Media weakBeat = new Media(getClass().getResource("/ml/echelon133/sounds/weak_beat.mp3").toExternalForm());
 
         IClickListener clickPlayer = new ClickPlayer(strongBeat, weakBeat);
+        IClickListener measureProgress = new MeasureProgress();
+
         Metronome metronome = new Metronome();
         // set done to true, otherwise metronome starts sending click events right after its thread start
         metronome.setDone(true);
+
         metronome.addClickListener(clickPlayer);
+
+        // measureProgress is both a listener and a wrapper for ObservableDoubleValue
+        // it has to listen to clicks, so that it can recalculate its value on every click
+        // it also has to be passed to the metronome instance as a field, so that metronome controller can get a reference to it
+        metronome.addClickListener(measureProgress);
+        metronome.setMeasureProgress((MeasureProgress)measureProgress);
 
         Thread metronomeThread = new Thread(metronome);
         metronomeThread.setDaemon(true);

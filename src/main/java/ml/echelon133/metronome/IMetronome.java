40c9c2bab9d225ba98a4aeb802277139222a0f3f
Mateusz Bednarski <echelon133@gmail.com>
2018-09-26 11:04:31 +0200

    Extracted static fields and non-static methods from Metronome concrete class into the interface

--- src/main/java/ml/echelon133/metronome/IMetronome.java
+++ src/main/java/ml/echelon133/metronome/IMetronome.java
@@ -1,12 +1,22 @@
 package ml.echelon133.metronome;
 
+import javafx.beans.value.ObservableDoubleValue;
+
 public interface IMetronome {
+    Integer MIN_BPM_VALUE = 50;
+    Integer MAX_BPM_VALUE = 240;
+    Integer DEFAULT_BPM_VALUE = 80;
+    Integer DEFAULT_ACCENT_INTERVAL = 4;
+
     void incrementBPMByOne();
     void decrementBPMByOne();
     void incrementBPMByFive();
     void decrementBPMByFive();
     long calculatePause();
     void setAccentInterval(Integer accentInterval);
+    void setMeasureProgress(ObservableDoubleValue measureProgress);
     Integer getBPM();
+    ObservableDoubleValue getProgress();
+    void setDone(Boolean b);
     void resetCounter();
 }
